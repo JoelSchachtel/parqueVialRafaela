8dc38e16687be9ab8ea15eef94b94645a3483896
Joel Schachtel <joelschachtel@hotmail.com>
2024-08-26 10:28:48 -0300

    Se incluyeron mejoras para mostrar la  inclusividad dentro del proyecto

--- scripts/main.js
+++ scripts/main.js
@@ -122,4 +122,14 @@ window.addEventListener("load", function () {
 if (faqs.length > 0) {
   faqs[0].classList.add('active');
 }
-;
+
+document.querySelectorAll('.image_container img').forEach(image => {
+  image.onclick = () => {
+    document.querySelector('.popup_image').style.display = "block";
+    document.querySelector('.popup_image img').src = image.getAttribute('src');
+  }
+
+  document.querySelector('.popup_image span').onclick = () => {
+    document.querySelector('.popup_image').style.display = "none";
+  }
+} )
